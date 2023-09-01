8af95f4b984a7c6890c26094aa0ee9ea5659c67a
walidbelharazem <walidbelharazem@gmail.com>
2023-09-01 18:48:41 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Code your solution in this file!
+/*  function returnFirstTwoDrivers(a) {
+    return a.slice(0, 2);
+  }
+  
+  function returnLastTwoDrivers(a) {
+    return a.slice(-2);
+  }
+  
+  function selectingDrivers(a, returnFirstTwoDrivers, returnLastTwoDrivers) {
+    return [returnFirstTwoDrivers(a), returnLastTwoDrivers(a)];
+  }
+  
+  module.exports = {
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers,
+    selectingDrivers, // Add selectingDrivers to the exported object
+  };*/
+
+  // helpers.js
+
+// Define the functions
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+  
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  }
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+  function createFareMultiplier(multiplier) {
+    return function (fare) {
+      return fare * multiplier;
+    };
+  }
+  
+  function fareDoubler(fare) {
+    return createFareMultiplier(2)(fare);
+  }
+  
+  function fareTripler(fare) {
+    return createFareMultiplier(3)(fare);
+  }
+  
+  function selectDifferentDrivers(drivers, fn) {
+    return fn(drivers);
+  }
+  
+  module.exports = {
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers,
+    selectingDrivers, 
+    createFareMultiplier,
+    fareDoubler,
+    fareTripler,
+    selectDifferentDrivers,
+  };
+  
